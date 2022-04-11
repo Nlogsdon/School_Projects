af1ed9ec1b51bfbf8c620b29c250f430e0d7cf5e
Nlogsdon <78663104+Nlogsdon@users.noreply.github.com>
2022-04-11 19:03:41 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        //initalize variables
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+        
+        //dispalys value of variables
+        System.out.println("j is " + j + " and" +" k is " + k +
+        " " + "i is " + i);
+    }
+}
